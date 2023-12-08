7eb67570fb07f495ee8bc4f2ee5750fba176fb34
MrSuricate <kevindu032270@gmail.com>
2023-12-08 15:48:43 +0100

    add import React from LanguagesContext.js

--- src/contextes/LanguagesContext.js
+++ src/contextes/LanguagesContext.js
@@ -1,4 +1,4 @@
-import { useMemo, useState } from "react";
+import React, { useMemo, useState } from "react";
 
 export const LanguagesContext = React.createContext(undefined);
 export function LanguagesProvider({children}) {
