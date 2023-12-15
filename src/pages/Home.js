46f11ae07bdeb9562dc2208d178f2d77773f750d
MrSuricate <kevindu032270@gmail.com>
2023-12-15 09:26:27 +0100

    Modification du Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,21 @@
 export default function Home() {
     return (
-        <div>{`Hello World`}</div>
+        <div className={"mt-20"}>
+            <div className={"flex flex-row gap-2 flex-wrap justify-between px-2"}>
+                <div>
+                    //todo select generation
+                </div>
+                <div>
+                    //todo select type
+                </div>
+                <div>
+                    //todo select order
+                </div>
+            </div>
+            <Modal/>
+            <div className={"grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 mt-2"}>
+                //todo mappé la liste de pokemon
+            </div>
+        </div>
     )
 }
